e30b7e52a26c6e8d009c9bad4437b7103d37d307
Erik <efdree@gmail.com>
2022-11-08 19:04:00 -0500

    Department card: style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Employee.destroy
+Department.destroy
+
+sales = Department.create(name: "Sales", description:"Department of Sales")
+
+erik = Employee.create(name: "Erik", email: "e@mail.com", password: "123456", department_id: 1)
